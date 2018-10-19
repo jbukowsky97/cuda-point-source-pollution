77fab10c69632f83d047dece5c274850082c7bd0
jbukowsky97 <j.bukowsky97@gmail.com>
2018-10-18 20:21:25 -0400

    Add subfunction to eliminate repeat code

--- cylinder.c
+++ cylinder.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned long long int convertToNum(char* str) {
+    const unsigned long long int temp = strtoull(str, NULL, 0);
+    if (temp < 0) {
+        printf("Inputs must be greater than 0\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(int argc, char** argv) {
-    if (argc != 6) {
-        printf("Usage:\n\t%s <cylinder size> <slice width> <time unit> <total time> <point source concentration>\n", argv[0]);
+    if (argc != 7) {
+        printf("Usage:\n\t%s <cylinder size> <slice width> <time unit> <total time> <point source concentration> <desired point>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    const unsigned long long int cylinderSize = strtoull(argv[1], NULL, 0);
-    if (cylinderSize < 0) {
-        printf("Cylinder size must be > 0\n");
+    const unsigned long long int cylinderSize = convertToNum(argv[1]);
+    const unsigned long long int sliceWidth = convertToNum(argv[2]);
+    const unsigned long long int timeUnit = convertToNum(argv[3]);
+    const unsigned long long int totalTime = convertToNum(argv[4]);
+    const unsigned long long int concentration = convertToNum(argv[5]);
+    const unsigned long long int desiredPoint = convertToNum(argv[6]);
+
+    if (timeUnit > totalTime) {
+        printf("Time unit must be less than total time\n");
+        exit(EXIT_FAILURE);
     }
 
-    const unsigned long long int sliceWidth = strtoull(argv[2], NULL, 0);
-    if (sliceWidth < 0) {
-        printf("Slice width must be > 0\n");
-    }
-
-    const unsigned long long int timeUnit = strtoull(argv[3], NULL, 0);
-    if (timeUnit < 0) {
-        printf("Time unit must be > 0\n");
-    }
-
-    const unsigned long long int totalTime = strtoull(argv[4], NULL, 0);
-    if (totalTime < 0) {
-        printf("Total time must be > 0\n");
-    }
-
-    const unsigned long long int concentration = strtoull(argv[5], NULL, 0);
-    if (concentration < 0) {
-        printf("Concentration must be > 0\n");
+    if (desiredPoint > cylinderSize) {
+        printf("Desired point must be less than cylinder size\n");
+        exit(EXIT_FAILURE);
     }
 }
