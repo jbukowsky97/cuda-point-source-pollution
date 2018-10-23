c110345cc7d290e94efba312a8394e9048306e1a
Dante Welch <welchdan@mail.gvsu.edu>
2018-10-22 21:27:06 -0400

    can't get it to parallelize

--- kernels.cu
+++ kernels.cu
@@ -4,6 +4,15 @@
 #include "kernels.cuh"
 
 __global__ void point_source_pollution_kernel(double *old_cylinder, double *new_cylinder, double *density, unsigned int n) {
-    //write our parallelized algorithm here amiright?
-    int x = 0;
+    
+    //the numner of computations each thread needs to do
+    unsigned int stride = blockDim.x * gridDim.x;
+
+    //shared cache, idk if this is exactly what we need
+    //but we will need some type of shared cache
+    __shared__ float cache[256];
+
+    //we want to make sure each thread does the correct 
+    //number of computations
+    
 }
