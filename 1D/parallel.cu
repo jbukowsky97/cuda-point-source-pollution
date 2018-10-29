c47338de396e4e8bb52a3ff04f775275a1666321
jbukowsky97 <j.bukowsky97@gmail.com>
2018-10-29 16:55:57 -0400

    Adjust 1D solution to support multiple impulses

--- 1D/parallel.cu
+++ 1D/parallel.cu
@@ -16,23 +16,28 @@ __global__ void calculateNext(double* oldCylinder, double* newCylinder, const un
     }
 }
 
-__global__ void initializeArray(double* cylinder, const unsigned long long int numSlices, const double concentration) {
+__global__ void initializeArray(double* cylinder, const unsigned long long int numSlices, const int numImpulses, const unsigned long long int* impulses, const double* concentrations) {
     int i = blockIdx.x * BLOCK_SIZE + threadIdx.x;
 
     if (i < numSlices) {
-        if (i == 0) {
-            cylinder[i] = concentration;
-        } else {
-            cylinder[i] = 0.0;
+        cylinder[i] = 0.0;
+        for (int k = 0; k < numImpulses; k++) {
+            if (i == impulses[k]) {
+                cylinder[i] = concentrations[k];
+                break;
+            }
         }
     }
 }
 
-extern "C" double gpuCalculate(const unsigned long long int numSlices, const unsigned long long int totalTime, const double concentration, const unsigned long long int desiredPoint) {
+extern "C" double gpuCalculate(const unsigned long long int numSlices, const unsigned long long int totalTime, const unsigned long long int desiredPoint, const int numImpulses, const unsigned long long int* impulses, const double* concentrations) {
     cudaError_t mallocResult;
     double* oldCylinder;
     double* newCylinder;
     double* temp;
+
+    unsigned long long int* deviceImpulses;
+    double* deviceConcentrations;
 
     mallocResult = cudaMalloc((void**) &oldCylinder, numSlices * sizeof(double));
     if (mallocResult != cudaSuccess) {
@@ -46,11 +51,35 @@ extern "C" double gpuCalculate(const unsigned long long int numSlices, const uns
         exit(EXIT_FAILURE);
     }
 
+    mallocResult = cudaMalloc((void**) &deviceImpulses, numImpulses * sizeof(unsigned long long int));
+    if (mallocResult != cudaSuccess) {
+        fprintf(stderr, "CUDA Malloc failed, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    mallocResult = cudaMalloc((void**) &deviceConcentrations, numImpulses * sizeof(double));
+    if (mallocResult != cudaSuccess) {
+        fprintf(stderr, "CUDA Malloc failed, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    mallocResult = cudaMemcpy(deviceImpulses, impulses, numImpulses * sizeof(unsigned long long int), cudaMemcpyHostToDevice);
+    if (mallocResult != cudaSuccess) {
+        fprintf(stderr, "CUDA Memcpy failed, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    mallocResult = cudaMemcpy(deviceConcentrations, concentrations, numImpulses * sizeof(double), cudaMemcpyHostToDevice);
+    if (mallocResult != cudaSuccess) {
+        fprintf(stderr, "CUDA Memcpy failed, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
     dim3 dimBlock(BLOCK_SIZE);
     unsigned long long int gridSize = ceil(numSlices / (double) BLOCK_SIZE);
     dim3 dimGrid(gridSize);
 
-    initializeArray<<<dimGrid, dimBlock>>>(oldCylinder, numSlices, concentration);
+    initializeArray<<<dimGrid, dimBlock>>>(oldCylinder, numSlices, numImpulses, deviceImpulses, deviceConcentrations);
 
     for (int i = 0; i < totalTime; i++) {
         calculateNext<<<dimGrid, dimBlock>>>(oldCylinder, newCylinder, numSlices);
@@ -73,6 +102,18 @@ extern "C" double gpuCalculate(const unsigned long long int numSlices, const uns
     }
 
     mallocResult = cudaFree(newCylinder);
+    if (mallocResult != cudaSuccess) {
+        fprintf(stderr, "CUDA free failed, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    mallocResult = cudaFree(deviceImpulses);
+    if (mallocResult != cudaSuccess) {
+        fprintf(stderr, "CUDA free failed, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    mallocResult = cudaFree(deviceConcentrations);
     if (mallocResult != cudaSuccess) {
         fprintf(stderr, "CUDA free failed, exiting...\n");
         exit(EXIT_FAILURE);
